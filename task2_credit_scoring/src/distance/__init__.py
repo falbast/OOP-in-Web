aa74f64103a5e7d983468a0b0e47276e84abbc7a
falbast <138040579+falbast@users.noreply.github.com>
2023-11-20 07:31:57 +0300

    Added task2

---
+++ task2_credit_scoring/src/distance/__init__.py
@@ -0,0 +1,11 @@
+from .chebyshev import ChebyshevDistance
+from .euclidean import EuclideanDistance
+from .manhattan import ManhattanDistance
+from .sorensen import SorensenDistance
+
+__all__ = (
+    "ChebyshevDistance",
+    "EuclideanDistance",
+    "ManhattanDistance",
+    "SorensenDistance",
+)
---
+++ task2_credit_scoring/src/distance/chebyshev.py
@@ -0,0 +1,13 @@
+from typing import Iterable
+
+from distance.base import BaseSampleDistance
+
+
+class ChebyshevDistance(BaseSampleDistance):
+    """Класс для определения расстояния между семплами (Чебышев)"""
+
+    k = 1
+
+    @staticmethod
+    def reduction(values: Iterable[float]) -> float:
+        return max(values)
