b6611b47d8ad983a4dfde5f8dd1a7d0ae05b1202
falbast <138040579+falbast@users.noreply.github.com>
2023-11-24 08:55:08 +0300

    Update app.py

--- task3_framework/webapp/app.py
+++ task3_framework/webapp/app.py
@@ -6,7 +6,24 @@ from storage import SampleStorage
 
 app = API()
 sample_storage = SampleStorage()
-sample_storage.create(seniority=9, home=1, time=60, age=30, marital=2, records=1, job=3, expenses=73, income=129, assets=0, debt=0, amount=800, price=846, predict=True)
+
+for _ in range(5):
+    sample_storage.create(
+        seniority=random.randint(0, 30), 
+        home=random.choice([x.value for x in HomeType]), 
+        time=random.randint(1, 60), 
+        age=random.randint(18, 90), 
+        marital=random.choice([x.value for x in MaritalType]), 
+        records=random.choice([True, False]), 
+        job=random.choice([x.value for x in JobType]), 
+        expenses=random.randint(0, 9999999), 
+        income=random.randint(0, 9999999), 
+        assets=random.randint(0, 99999999), 
+        debt=random.randint(0, 999999), 
+        amount=random.randint(0, 999999), 
+        price=random.randint(0, 999999)
+    )
+
 app.add_middleware(TokenMiddleware)
 app.add_exception_handler(on_exception)
 
